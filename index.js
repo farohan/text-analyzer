57241ee6eda0131be03ac11f638ffe3384e24b86
farohan <68710283+farohan@users.noreply.github.com>
2022-06-17 13:43:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 //Welcome to index.js!
 
 const paper = document.getElementById('paper');
-const submit = document.getElementById('submit');
+const analyze = document.getElementById('analyze');
 const results = document.getElementById('results');
 
 const textAnalyzer = {
     name: 'Text Analyzer 1.0',
     charCount: () => {
-        console.log(paper.value.length);
+        return paper.value.length;
     },
     wordCount: () => {
-        console.log(Math.round(paper.value.length / 5));
+        let trimmedPaper = paper.value.trim();
+        return Math.round(trimmedPaper.length / 5);
     },
     longestWord: (paper) => {
         let sorted = paper.value
             .split(' ')
             .sort((wordA, wordB) => wordB.length - wordA.length);
         return sorted[0];
+    },
+    vowelCounter: (paper) => {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        let count = 0;
+
+        for (let letter of paper.value.toLowerCase()) {
+            if (vowels.includes(letter)) 
+                count++;
+        }
+
+        return count;
     },
     bagOfWords: () => {
     },
@@ -28,3 +41,21 @@ const textAnalyzer = {
 document.title = textAnalyzer.name;
 const heading = document.querySelector('h1');
 heading.innerHTML = textAnalyzer.name;
+
+analyze.addEventListener('click', showResults);
+
+function showResults() {
+    results.innerHTML = `
+      <strong> Character Count (incl. spaces): </strong> ${textAnalyzer.charCount()}
+      <br>
+      <strong> Word Count: </strong> ${textAnalyzer.wordCount()}
+      <br>
+      <strong> Longest Word: </strong> ${textAnalyzer.longestWord(paper)}
+      <br>
+      <strong> Number of Vowels: </strong> ${textAnalyzer.vowelCounter(paper)}
+      <br>
+      <strong> Bag of Words: </strong> ${textAnalyzer.bagOfWords()}
+      <br>
+      <strong> Number of Unique Words: </strong> ${textAnalyzer.uniqueWords()}
+    `
+}
