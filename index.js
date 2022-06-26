ccb581e4a41794c1b77f33a2d65c2fb399b4c843
farohan <68710283+farohan@users.noreply.github.com>
2022-06-25 17:52:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //Welcome to index.js!
 
+import { fillerWords } from "./box.js";
+
 const paper = document.getElementById('paper');
 const analyze = document.getElementById('analyze');
-const results = document.getElementById('results');
+const generalData = document.getElementById('data');
+const assumptions = document.getElementById('assumptions');
 
 const textAnalyzer = {
     name: 'Text Analyzer 1.0',
@@ -49,9 +52,41 @@ const textAnalyzer = {
 
         return count;
     },
-    bagOfWords: () => {
+    vToCRatio: () => {
+        return textAnalyzer.vowelCounter(paper) / textAnalyzer.consonantCounter(paper);
+    },
+    cToVRatio: () => {
+        return textAnalyzer.consonantCounter(paper) / textAnalyzer.vowelCounter(paper);
+    },
+    bagOfWords: () => {      
+        return fillerWords.length;
     },
     uniqueWords: () => {
+    }
+};
+
+const judge = {
+    name: 'Text Judger',
+    lengthJudger: () => {
+        if (textAnalyzer.wordCount(paper) <= 75)
+            return 'very short';
+        else if (textAnalyzer.wordCount(paper) <= 150) 
+            return 'short';
+        else if (textAnalyzer.wordCount(paper) <= 500)
+            return 'medium-sized';
+        else if (textAnalyzer.wordCount(paper) <= 2000)
+            return 'long';
+        else return 'very long';
+    },
+    vowelComparison: () => {
+        if (textAnalyzer.vToCRatio() > textAnalyzer.cToVRatio()) 
+            return 'vowels than consonants';
+        else return 'consonants than vowels';
+    },
+    guessedLanguage: () => {
+        if (judge.vowelComparison() === 'vowels than consonants')
+            return 'Spanish, Portuguese, Italian, or any other vowel-filled language'
+        else return 'English or other languages similar to English'
     }
 };
 
@@ -62,7 +97,9 @@ heading.innerHTML = textAnalyzer.name;
 analyze.addEventListener('click', showResults);
 
 function showResults() {
-    results.innerHTML = `
+    generalData.innerHTML = `
+      <em> <h2> General Data </h2> </em>
+
       <strong> Character Count (incl. spaces): </strong> ${textAnalyzer.charCount()}
       <br>
       <strong> Character Count (excl. spaces): </strong> ${textAnalyzer.exclSpace()}
@@ -75,8 +112,24 @@ function showResults() {
       <br>
       <strong> Number of Consonants: </strong> ${textAnalyzer.consonantCounter(paper)}
       <br>
+      <strong> Vowel to Consonant Ratio: </strong> ${textAnalyzer.vToCRatio()}
+      <br>
+      <strong> Consonant to Vowel Ratio: </strong> ${textAnalyzer.cToVRatio()}
+      <br>
       <strong> Bag of Words: </strong> ${textAnalyzer.bagOfWords()}
       <br>
       <strong> Number of Unique Words: </strong> ${textAnalyzer.uniqueWords()}
-    `
+    `;
+    assumptions.innerHTML  = `
+        <em> <h2> Assumptions </h2> </em>
+
+        This is a ${judge.lengthJudger()} text.
+        Looks like it has more ${judge.vowelComparison()}.
+        I'm guessing it's ${judge.guessedLanguage()}.
+        The bag of words that I found tells me that this text is about the following: _____.
+        Also, the number of unique words tells me that whoever wrote this has a ____ vocabulary.
+    `;
+
+    //interpretation
+    //vocabularyJudger
 }
